72e29ef30132cf68b57955f3635c60987765c699
Robert Nastase <robertionut0609@gmail.com>
2021-12-08 12:50:25 +0200

    Signed-off-by: Robert Nastase <robertionut0609@gmail.com>
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	new file:   COPYING
    	new file:   COPYING.LESSER
    	modified:   README.txt
    	modified:   bot-skel.py

--- bot-skel.py
+++ bot-skel.py
@@ -1,4 +1,4 @@
-
+# Copyright Nastase Robert-Ionut 312CA
     
 
     #!./.venv/bin/python
@@ -159,3 +159,21 @@ if __name__ == '__main__':
 
     # launch bot (blocking operation)
     bot.run(os.environ['BOT_TOKEN'])
+  
+"""
+    Discord Music Bot
+    Copyright (C) 2021  Robert Nastase
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
